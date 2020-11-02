116866182cb01ca5f30b212f1874bc87695b4eb1
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-02 10:40:41 -0800

    add additional functions to business logic
    Co-authored-by: Cory Nordenbrock <cordenbrock@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,66 @@
-// constructor
+
+// Business Logic for AddressBook -------------
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Contacts -------------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
-
-let johnDoe = new Contact("John", "Doe", "1234567");
-console.log(johnDoe)
-
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
+
+
+    // deleteContact function
+
+// UI logic
+
+    // generate address book
+    let addressBook = new AddressBook();
+
+    // displayContactDetails function
+    // showContact function
+    // attachContactListeners function
+
+    // (doc).ready interactivity
